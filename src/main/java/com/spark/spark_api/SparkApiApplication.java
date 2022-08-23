0f292b4056105711760d44380f45711a7579879a
YueXing1 <79831842+YueXing1@users.noreply.github.com>
2022-08-22 17:27:35 -0700

    Add files via upload

---
+++ src/main/java/com/spark/spark_api/SparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.spark.spark_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkApiApplication.class, args);
+	}
+
+}
